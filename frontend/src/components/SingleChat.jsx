92a2d3fe5cdff5dd5d035ba99bead4f346f84111
deepti2820 <165156846+deepti2820@users.noreply.github.com>
2024-09-13 00:07:19 +0530

    Update SingleChat.jsx

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -12,7 +12,7 @@ import io from "socket.io-client";
 import animationData from "../animations/typing.json"
 import ScrollableChat from './ScrollableChat';
 import { StoreContext } from '../context/StoreContext';
-const ENDPOINT="http://localhost:5000"
+const ENDPOINT="https://causegrambackend-bju0.onrender.com"
 var socket,selectedChatCompare;
 
 function SingleChat({fetchAgain,setFetchAgain}) {
